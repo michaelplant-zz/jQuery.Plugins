51c43ec7f01ad6d760830192a9739d61e9d3e5c7
Mike Plant <msplant@gmail.com>
2012-01-23 11:11:13 -0500

    linking to and adding first plugin

---
+++ assets/js/jquery.featured-content.js
@@ -0,0 +1,132 @@
+(function( $ ){
+	
+	// Public Properties
+	var properties = {
+		tags		: {},
+		total		: function() {
+						return properties.tags.length;
+					  },
+		activeIndex	: 0
+	}
+	
+	// Create some default options, merged in init
+	var settings = {
+		repeaterTag			: 'img',
+		animation			: 'crossFade',
+		showPaging			: true,
+		pagingPlacement		: 'bottom-right',
+		startIndex			: 0,
+		speed				: 4500
+	}
+	
+	var methods = {
+		init: function( options ) {
+			
+			// merge the settings with any options that were provided
+			settings = $.extend( settings, options);
+			
+			return this.each(function(){
+
+				// hold onto "this" for interval
+				var self = this;
+				
+				// get items
+				properties.tags = $(self).find(settings.repeaterTag.toLowerCase());
+				
+				// wait for images to load completely, before initializing
+				$(window).bind('load', function() {
+					
+					console.log('windowloaded');
+				
+					properties.maxSize = getMaximumSize(properties.tags);
+					
+					// set basic css rules
+					$(self)
+						.css({
+							position	: 'relative',
+							width		: properties.maxSize.width,
+							height		: properties.maxSize.height,
+							overflow	: 'hidden'
+						});
+					
+					// other css, and set starting slide to active
+					properties.tags
+						.css({
+								position	: 'absolute',
+								width		: properties.maxSize.width,
+								height		: properties.maxSize.height,
+								display		: 'none'
+							})
+							.addClass('featured-content-' + settings.repeaterTag.toLowerCase())
+						.eq(settings.startIndex)
+							.css({
+								display		: 'block'
+							})
+							.addClass('active');
+							
+					// start timer
+					setInterval(function(){ 
+					
+						// save active/next indexies
+						properties.activeIndex = $(self).find(settings.repeaterTag + '.active').index();
+						var next = properties.activeIndex + 1;
+						properties.nextIndex = (properties.total() == next) ? 0 : next;
+						//console.log(properties.activeIndex,properties.nextIndex,properties.total());
+					
+						// each animation should position css according to the animation to occur
+						switch ( settings.animation ) {
+							case 'slide':
+								slideLeft();
+							break;
+							default:
+								crossFade();
+							break;
+						}
+					}, settings.speed);
+					
+				});
+				
+			}); 
+
+		}
+	};
+	// animates with a slide effect
+	function slideLeft() {
+		properties.tags.css('z-index',-1);
+		properties.tags.eq(properties.activeIndex).css({ zIndex: 0 }).removeClass('active');
+		properties.tags.eq(properties.nextIndex).css({ display: 'block', left: properties.maxSize.width, zIndex: 1 }).animate({left:0},750).addClass('active');
+	}
+	// animates with a crossfade
+	function crossFade() {
+		properties.tags.eq(properties.activeIndex).fadeOut().removeClass('active');
+		properties.tags.eq(properties.nextIndex).fadeIn().addClass('active');
+	}
+	// loops through the elements set to obtain the greater height
+	function getMaximumSize(elemsSet){
+		var maxWidth = 0;
+		var maxHeight = 0;
+		elemsSet.each(function(){
+			item = $(this);
+			maxHeight = Math.max( item.height(), maxHeight );
+			maxWidth = Math.max( item.width(), maxWidth );
+			childItems = getMaximumSize(item.children());
+			maxHeight = Math.max( childItems.height, maxHeight );
+			maxWidth = Math.max( childItems.width, maxWidth );
+		});
+		return {width: maxWidth, height:maxHeight};
+	}
+	
+	$.fn.featuredContent = function( method ) {  
+	
+		// Method calling logic
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method: "' +  method + '" does not exist on jQuery.featuredContent' );
+		}
+			
+	};
+	
+})( jQuery );
