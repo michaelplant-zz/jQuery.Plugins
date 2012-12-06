6c536a026997bf35795026fdcaad75fe22989e22
Mike Plant <msplant@gmail.com>
2012-12-06 15:58:24 -0500

    added clicking to paging

--- assets/js/jquery.featured-content.js
+++ assets/js/jquery.featured-content.js
@@ -1,12 +1,13 @@
 (function ($) {
-
+	var self = {};
     // Public Properties
     var properties = {
         tags: {},
         total: function () {
             return properties.tags.length;
         },
-        activeIndex: 0
+        activeIndex: 0,
+        timer: {},
     }
 
     // Create some default options, merged in init
@@ -31,7 +32,7 @@
             return this.each(function () {
 
                 // hold onto "this" for interval
-                var self = this;
+                self = this;
 
                 // get items
                 properties.tags = $(self).children(settings.repeaterTag.toLowerCase()).not(settings.exclude);
@@ -67,25 +68,8 @@
 							})
 							.addClass('active');
 
-                    // start timer
-                    setInterval(function () {
-
-                        // save active/next indexies
-                        properties.activeIndex = $(self).find(settings.repeaterTag + '.active').index();
-                        var next = properties.activeIndex + 1;
-                        properties.nextIndex = (properties.total() == next) ? 0 : next;
-                        //console.log(properties.activeIndex,properties.nextIndex,properties.total());
-
-                        // each animation should position css according to the animation to occur
-                        switch (settings.animation) {
-                            case 'slide':
-                                slideLeft();
-                                break;
-                            default:
-                                crossFade();
-                                break;
-                        }
-                    }, settings.speed);
+                    // settings eventually - this should be an option
+                    startSlideShow();
 
                     if (settings.showPaging) {
                         generatePaging(self);
@@ -96,17 +80,80 @@
 
             });
 
-        }
+        },// end init
+        advance: function() {
+        	animateForward();
+        },
+        retreat: function() {
+        	animateBackward();
+        },
     };
+    // move slideshow forward
+    function animateForward() {
+    	switch (settings.animation) {
+			case 'slide':
+				slide();
+				break;
+			default:
+				crossFade();
+				break;
+		}
+    }
+    // move slideshow backward
+    function animateBackward() {
+    	console.log('figure out going backward - not coded yet');
+    	/*switch (settings.animation) {
+			case 'slide':
+				slide();
+				break;
+			default:
+				crossFade();
+				break;
+		}*/
+    }    
+    // start the slideshow
+    function startSlideShow() {
+    	// start timer
+		properties.timer = setInterval(function () {
+			// save active/next indexies
+			properties.activeIndex = $(self).find(settings.repeaterTag + '.active').index();
+			var next = properties.activeIndex + 1;
+			properties.nextIndex = (properties.total() == next) ? 0 : next;
+			//console.log(properties.activeIndex,properties.nextIndex,properties.total());
+			
+			// call animation to step forward 
+			animateForward();
+			
+		}, settings.speed);
+    }    
+    // start the slideshow
+    function stopSlideShow() {
+    	clearTimeout(properties.timer);
+    }
     // animates with a slide effect
-    function slideLeft() {
-        properties.tags.css({'z-index': -1, left: '100%'});
-        properties.tags.eq(properties.activeIndex).css({ zIndex: 0, left: 0 }).removeClass('active');
-        properties.tags.eq(properties.nextIndex).css({ display: 'block', zIndex: 1 }).delay(0).animate({ left: 0 }, 750).addClass('active');
+    function slide(toIndex) {
+    	if ( toIndex == undefined ) {
+    		toIndex = properties.nextIndex;
+    	} else {
+    		stopSlideShow();
+    		startSlideShow();
+    	}
+        if ( toIndex > properties.activeIndex || toIndex == 0 ) {
+        	// forward
+			properties.tags.css({'z-index': -1, left: '100%'});
+			properties.tags.eq(properties.activeIndex).css({ zIndex: 0, left: 0 }).removeClass('active');
+			properties.tags.eq(toIndex).css({ display: 'block', zIndex: 1 }).delay(0).animate({ left: 0 }, 750).addClass('active');
+        } else {
+        	// backward
+			properties.tags.css({'z-index': -1, left: '-100%'});
+			properties.tags.eq(properties.activeIndex).css({ zIndex: 0, left: 0 }).removeClass('active');
+			properties.tags.eq(toIndex).css({ display: 'block', zIndex: 1 }).delay(0).animate({ left: 0 }, 750).addClass('active');
+        }
         if (settings.showPaging) {
             properties.paging.eq(properties.activeIndex).removeClass('active');
-            properties.paging.eq(properties.nextIndex).addClass('active');
+            properties.paging.eq(toIndex).addClass('active');
         }
+        properties.activeIndex = toIndex;
     }
     // animates with a crossfade
     function crossFade() {
@@ -136,7 +183,7 @@
     	
         var pageHTML = '<div class="featured-content-paging">';
         properties.tags.each(function (index, elem) {
-            pageHTML += '<span class="page" style="width:' + settings.pagingWidth + 'px; height:' + settings.pagingHeight + 'px">&nbsp;</span>';
+            pageHTML += '<span class="page" style="width:' + settings.pagingWidth + 'px; height:' + settings.pagingHeight + 'px; cursor:pointer;" data-index="'+index+'">&nbsp;</span>';
         });
         pageHTML += '</div>';
         $(container)
@@ -144,6 +191,10 @@
             .find('.page:eq(' + properties.activeIndex + ')')
             .addClass('active');
         properties.paging = $(container).find('.page');
+        // add click handler to paging spans
+        properties.paging.bind('click',function(e){
+        	slide($(e.target).data('index'));
+        });
         return;
     }
 
